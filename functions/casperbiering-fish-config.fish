4441268170b311c1b2b4b94311f7050b7b87e362
Casper Biering <cb@peercraft.com>
2018-09-04 23:17:39 +0200

    initial commit

---
+++ functions/casperbiering-fish-config.fish
@@ -0,0 +1,4 @@
+function casperbiering-fish-config -d "My package"
+  # Package entry-point
+  echo he2llo
+end
---
+++ init.fish
@@ -0,0 +1,22 @@
+# casperbiering-fish-config initialization hook
+#
+# You can use the following variables in this file:
+# * $package       package name
+# * $path          package path
+# * $dependencies  package dependencies
+
+set -g theme_display_git_master_branch yes
+set -g theme_display_vagrant no
+set -g theme_title_display_process yes
+set -g theme_display_k8s_context no
+set -g theme_display_virtualenv no
+set -g theme_display_hg no
+set -g theme_display_ruby no
+set -g theme_display_vi no
+set -g theme_show_exit_status yes
+#set -g theme_title_display_user yes
+set -g fish_prompt_pwd_dir_length 0
+set -g theme_color_scheme dark
+set -g theme_display_git_ahead_verbose yes
+set -g theme_display_git_dirty_verbose yes
+#set -g theme_powerline_fonts no
